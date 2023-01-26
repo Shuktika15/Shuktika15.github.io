6f0200ec4d51e69a37696042923e4de5ae7ea3e7
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-01-26 23:00:44 +0530

    Move App component to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './main.scss'
+import Header from "./components/header/Header";
+import Landing from "./components/landing/Landing";
+import AboutMe from "./components/about-me/AboutMe";
+import Skills from "./components/skills/Skills";
+import Experiences from "./components/experiences/Experiences";
+import Educations from "./components/educations/Educations";
+
+function App() {
+    return (
+        <>
+            <Header/>
+            <main>
+                <Landing/>
+                <AboutMe/>
+                <Skills/>
+                <Experiences/>
+                <Educations/>
+            </main>
+            <footer></footer>
+        </>
+    )
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 )
