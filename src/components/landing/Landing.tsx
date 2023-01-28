37e165bb765c31e7d46f85addc8ef315b45dd4a4
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-01-29 00:46:48 +0530

    Add social icons

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,4 +1,10 @@
 import "./Landing.scss";
+import github from "../../assets/icons/github.svg";
+import linkedin from "../../assets/icons/linkedin.svg";
+import facebook from "../../assets/icons/facebook.svg";
+import instagram from "../../assets/icons/instagram.svg";
+import pinterest from "../../assets/icons/pinterest.svg";
+import leetcode from "../../assets/icons/leetcode.svg";
 
 export default function Landing() {
     return (
@@ -14,6 +20,26 @@ export default function Landing() {
                         a success.
                     </b>
                 </p>
+                <div className="socials">
+                    <a href="https://github.com/Shuktika15">
+                        <img src={github} alt="GitHub"/>
+                    </a>
+                    <a href="https://in.linkedin.com/in/shuktika-mahanty">
+                        <img src={linkedin} alt="LinkedIn"/>
+                    </a>
+                    <a href="https://www.facebook.com/ShuktikaMahanty">
+                        <img src={facebook} alt="Facebook"/>
+                    </a>
+                    <a href="https://www.instagram.com/shuktikamahanty">
+                        <img src={instagram} alt="Instagram"/>
+                    </a>
+                    <a href="https://pinterest.com/shuktikam">
+                        <img src={pinterest} alt="Pinterest"/>
+                    </a>
+                    <a href="https://leetcode.com/shukti">
+                        <img src={leetcode} alt="LeetCode"/>
+                    </a>
+                </div>
             </div>
         </section>
     )
