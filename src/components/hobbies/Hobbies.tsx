49daf02d4b0f237f8d3f3c5f13b8e6a94002bf7e
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-01-28 23:11:37 +0530

    Create digital painting hobby

--- src/components/hobbies/Hobbies.tsx
+++ src/components/hobbies/Hobbies.tsx
@@ -11,7 +11,26 @@ export default function Hobbies() {
                     eveniet, facilis fugiat harum illum incidunt laboriosam minus modi molestias natus nihil non optio
                     perferendis quae qui quia quibusdam, quo quod recusandae repudiandae sed sint temporibus.
                 </p>
-                <div className="collage">
+                <div className="collage css-arts">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </article>
+            <article className="hobby">
+                <h3 className="color-accent">Digital Painting</h3>
+                <p className="content">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aliquam cumque, error esse
+                    eveniet, facilis fugiat harum illum incidunt laboriosam minus modi molestias natus nihil non optio
+                    perferendis quae qui quia quibusdam, quo quod recusandae repudiandae sed sint temporibus.
+                </p>
+                <div className="collage digital-paintings">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                     <div></div>
                     <div></div>
                     <div></div>
