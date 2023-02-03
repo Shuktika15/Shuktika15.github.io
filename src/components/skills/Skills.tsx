affbb071e54f28e18c0322e1cf9076f3926a19d5
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-03 10:56:29 +0530

    Use dynamic icons for skills

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,12 +4,18 @@ import {Data, Skill} from "../../data";
 export default function Skills() {
     const skills: Skill[] = Data.skills;
 
+    function getLightImage(logoName: string): string {
+        return new URL(`/public/icons/${logoName}-light.svg`, import.meta.url).href;
+    }
+
     return (
         <section id="Skills">
             <h1>Skills</h1>
             {skills.map((skill) => (
                 <article className="skill relative transition has-top" key={skill.id}>
-                    <div className="logo"></div>
+                    <div className="logo">
+                        <img src={getLightImage(skill.logo)} alt={skill.logo}/>
+                    </div>
                     <h3>{skill.title}</h3>
                     {skill.descriptions.map((description) => (
                         <p className="content" key={description}>{description}</p>
