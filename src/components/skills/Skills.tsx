ec45850a4b857e6e01e3e411c7cab2135a2d21da
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-03 21:43:16 +0530

    Remove the use of urls to prevent looking for in public folder

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -4,17 +4,13 @@ import {Data, Skill} from "../../data";
 export default function Skills() {
     const skills: Skill[] = Data.skills;
 
-    function getLightImage(logoName: string): string {
-        return new URL(`/public/icons/skills/${logoName}-light.svg`, import.meta.url).href;
-    }
-
     return (
         <section id="Skills">
             <h1>Skills</h1>
             {skills.map((skill) => (
                 <article className="skill relative transition has-top" key={skill.id}>
                     <div className="logo">
-                        <img loading="lazy" src={getLightImage(skill.logo)} alt={skill.logo}/>
+                        <img loading="lazy" src={`/icons/skills/${skill.logo}-light.svg`} alt={skill.logo}/>
                     </div>
                     <h3>{skill.title}</h3>
                     {skill.descriptions.map((description) => (
