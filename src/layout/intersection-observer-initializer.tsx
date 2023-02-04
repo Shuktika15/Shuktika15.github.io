9774161ba52da8ac19b66c4e18614e96d6753b91
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-04 17:29:02 +0530

    User HTMLElement instead of Element

--- src/layout/intersection-observer-initializer.tsx
+++ src/layout/intersection-observer-initializer.tsx
@@ -5,7 +5,7 @@ export default function IntersectionObserverInitializer() {
         threshold: 0.5
     };
 
-    function executor(elements: NodeListOf<Element>, callback: (element: Element, index: number) => void) {
+    function executor(elements: NodeListOf<HTMLElement>, callback: (element: HTMLElement, index: number) => void) {
         for (let i = 0; i < elements.length; ++i) {
             const element = elements[i];
             callback(element, i);
@@ -13,8 +13,8 @@ export default function IntersectionObserverInitializer() {
     }
 
     useEffect(() => {
-        const skills = document.querySelectorAll(".skill");
-        const projects = document.querySelectorAll(".project");
+        const skills = document.querySelectorAll<HTMLElement>(".skill");
+        const projects = document.querySelectorAll<HTMLElement>(".project");
         const observer = new IntersectionObserver((entries) => {
             for (const entry of entries) {
                 entry.target.classList.toggle("no-top", entry.isIntersecting);
@@ -25,11 +25,11 @@ export default function IntersectionObserverInitializer() {
             }
         }, intersectionOptions);
 
-        executor(skills, (element: Element, index: number) => {
+        executor(skills, (element: HTMLElement, index: number) => {
             element.style.transitionDelay = `${index * 250}ms`;
             observer.observe(element);
         });
-        executor(projects, (element: Element) => {
+        executor(projects, (element: HTMLElement) => {
             observer.observe(element);
         });
     }, []);
