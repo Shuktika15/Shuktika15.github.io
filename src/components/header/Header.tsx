0a4d0f21014c61f23319a40771d0caf9aa5e0c08
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-02-10 15:57:40 +0530

    Create nav

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,7 +4,29 @@ export default function Header() {
     return (
         <header className="rotate-y">
             <div className="container">
-                <h1>Shuktika<span>.</span></h1>
+                <a href="/#Landing">
+                    <h1>Shuktika<span>.</span></h1>
+                </a>
+                <button className="nav-btn transition">
+                    <i className="fa-solid fa-bars"></i>
+                </button>
+                <nav>
+                    <a href="/#AboutMe">
+                        <p>About Me</p>
+                    </a>
+                    <a href="/#Skills">
+                        <p>Skills</p>
+                    </a>
+                    <a href="/#Experiences">
+                        <p>Experiences</p>
+                    </a>
+                    <a href="/#Projects">
+                        <p>Projects</p>
+                    </a>
+                    <a href="/#Hobbies">
+                        <p>Hobbies</p>
+                    </a>
+                </nav>
             </div>
         </header>
     );
