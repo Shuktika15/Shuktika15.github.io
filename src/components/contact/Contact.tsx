b8e46990942d022e80aeb3f4940f587f8f573726
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-05 19:48:05 +0530

    Change button style

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,14 +1,22 @@
 import "./Contact.scss";
+import {FormEvent, useRef} from "react";
 
 export default function Contact() {
+    const form = useRef<HTMLFormElement | null>(null);
+
+    function sendEmail(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+
     return (
         <section id="Contact">
             <h1>Contact</h1>
-            <form autoComplete="on">
-                <input type="text" name="name" placeholder="Name" spellCheck="false" autoComplete="on"/>
-                <input type="email" name="email" placeholder="Email" spellCheck="false" autoComplete="on"/>
-                <input type="text" name="subject" placeholder="Subject"/>
-                <textarea name="content" placeholder="Content"></textarea>
+            <form autoComplete="on" onSubmit={sendEmail}>
+                <input type="text" name="name" placeholder="Name" spellCheck="false" autoComplete="on" required/>
+                <input type="email" name="email" placeholder="Email" spellCheck="false" autoComplete="on" required/>
+                <input type="text" name="subject" placeholder="Subject" required/>
+                <textarea name="content" placeholder="Content" required></textarea>
+                <button type="submit" className="btn">Send</button>
             </form>
         </section>
     );
