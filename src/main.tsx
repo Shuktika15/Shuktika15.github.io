e5cdf9e65030106754ef6ae97d8690d5316a9885
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-03-18 16:50:52 +0530

    Change folder name from components to sections

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/main.scss'
-import Header from "./components/header/Header";
-import Landing from "./components/landing/Landing";
-import AboutMe from "./components/about-me/AboutMe";
-import Skills from "./components/skills/Skills";
-import Experiences from "./components/experiences/Experiences";
-import Educations from "./components/educations/Educations";
-import Projects from "./components/projects/Projects";
-import Hobbies from "./components/hobbies/Hobbies";
-import InitialLoading from "./components/initial-loading/InitialLoading";
-import Footer from "./components/footer/Footer";
-import Contact from "./components/contact/Contact";
+import Header from "./sections/header/Header";
+import Landing from "./sections/landing/Landing";
+import AboutMe from "./sections/about-me/AboutMe";
+import Skills from "./sections/skills/Skills";
+import Experiences from "./sections/experiences/Experiences";
+import Educations from "./sections/educations/Educations";
+import Projects from "./sections/projects/Projects";
+import Hobbies from "./sections/hobbies/Hobbies";
+import InitialLoading from "./sections/initial-loading/InitialLoading";
+import Footer from "./sections/footer/Footer";
+import Contact from "./sections/contact/Contact";
 
 function App() {
     return (
