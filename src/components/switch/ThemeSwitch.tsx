2f8275e265e226f99e9055f149ea49f92cc6e1b4
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2023-06-09 03:16:10 +0530

    Save theme settings to loacal storage

--- src/components/switch/ThemeSwitch.tsx
+++ src/components/switch/ThemeSwitch.tsx
@@ -1,9 +1,53 @@
 import { type JSX } from 'preact'
 import './ThemeSwitch.scss'
+import { useEffect, useState } from 'preact/compat'
+import { getFromLocalStorage, saveInLocalStorage } from '../../services/local-storage'
+import { LOCAL_STORAGE_KEY, THEME } from '../../constant'
 
 export default function ThemeSwitch (): JSX.Element {
+  const [initialLoad, setInitialLoad] = useState<boolean>(true)
+  const [theme, setTheme] = useState<THEME>(THEME.LIGHT)
+
+  useEffect(() => {
+    if (initialLoad) {
+      onLoad()
+      setInitialLoad(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    saveInLocalStorage(LOCAL_STORAGE_KEY.THEME, theme)
+  }, [theme])
+
+  function onLoad (): void {
+    const savedTheme = getFromLocalStorage(LOCAL_STORAGE_KEY.THEME)
+
+    if (savedTheme === null) {
+      if (isDarkTheme()) {
+        changeTheme()
+      } else {
+        saveInLocalStorage(LOCAL_STORAGE_KEY.THEME, THEME.LIGHT)
+      }
+    } else {
+      if (savedTheme === THEME.DARK) {
+        changeTheme()
+      }
+    }
+  }
+
+  function isDarkTheme (): boolean {
+    console.log(window.matchMedia?.('(prefers-color-scheme: dark)').matches)
+    return window.matchMedia?.('(prefers-color-scheme: dark)').matches
+  }
+
   function changeTheme (): void {
     document.body.classList.toggle('dark-theme')
+
+    if (theme === THEME.LIGHT) {
+      setTheme(THEME.DARK)
+    } else {
+      setTheme(THEME.LIGHT)
+    }
   }
 
   return (
---
+++ src/constant.ts
@@ -0,0 +1,8 @@
+export enum LOCAL_STORAGE_KEY {
+  THEME = 'theme'
+}
+
+export enum THEME {
+  LIGHT = 'LIGHT',
+  DARK = 'DARK'
+}
