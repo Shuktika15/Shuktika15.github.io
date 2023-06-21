b895006d080f2126712f825fa7d75d9dffc21be9
Shuktika15 <53374048+Shuktika15@users.noreply.github.com>
2023-06-21 19:07:30 +0000

    Deploying to gh-pages from @ Shuktika15/Shuktika15.github.io@2d6da9600e2254c780e6f43c453c250c683e7cd8 🚀

--- assets/index-ab23e9d6.js
+++ assets/index-ab23e9d6.js
@@ -4,13 +4,13 @@
                 auto-configuration, dependency management, and seamless integration
                 with other Spring ecosystem components.`]},{id:3,logo:"mysql",title:"MySQL",descriptions:[`Desirability of MySQL stems from its reliability, scalability, 
                 performance, ease of use, strong community support, and extensive 
-                compatibility with various platforms and programming languages.`]}],experiences:[{id:1,company:"Applied Reserach Works",startMonth:"Feb",startYear:2023,current:!0,descriptions:[`Worked in Node.js, Express.js and still learning 
-                Working on implementing a better grid system to replace 
-                the old one, which can drastically improve the performance 
-                and add a whole set of new features.`]},{id:2,company:"Cognizant",startMonth:"Oct",startYear:2020,current:!1,endMonth:"Feb",endYear:2023,descriptions:[`Worked on multiple insurance based client projects in
+                compatibility with various platforms and programming languages.`]}],experiences:[{id:1,company:"Applied Reserach Works",startMonth:"Feb",startYear:2023,current:!0,descriptions:[`Worked in Node.js, Express.js and still learning and 
+                improving on them. Working on implementing a better grid 
+                system to replace the old one, which can drastically 
+                improve the performance and add a whole set of new features.`]},{id:2,company:"Cognizant",startMonth:"Oct",startYear:2020,current:!1,endMonth:"Feb",endYear:2023,descriptions:[`Worked on multiple insurance based client projects in
                 upgrade team and development team.
                 Increased efficiency by 90% by automating the process of
-                finding upgrade files`]}],educations:[{id:1,institution:"Government College of Leather Technology",educationType:"B. Tech in Information Technology",gradeType:"cgpa",grade:8.34,startYear:2016,endYear:2020},{id:2,institution:"B.D.M. International",educationType:"CBSE 12th standard Board Exam",gradeType:"percentage",grade:89,startYear:2014,endYear:2016},{id:3,institution:"B.D.M. International",educationType:"CBSE 10th standard Board Exam",gradeType:"cgpa",grade:10,startYear:2012,endYear:2014}],projects:[{id:1,title:"Pension Management System",background:"pension-management.webp",github:"https://github.com/Shuktika15/pension-management",descriptions:[`A full stack application to calculate pension given the Aadhar Number of a pensioner. 
+                finding upgrade files.`]}],educations:[{id:1,institution:"Government College of Leather Technology",educationType:"B. Tech in Information Technology",gradeType:"cgpa",grade:8.34,startYear:2016,endYear:2020},{id:2,institution:"B.D.M. International",educationType:"CBSE 12th standard Board Exam",gradeType:"percentage",grade:89,startYear:2014,endYear:2016},{id:3,institution:"B.D.M. International",educationType:"CBSE 10th standard Board Exam",gradeType:"cgpa",grade:10,startYear:2012,endYear:2014}],projects:[{id:1,title:"Pension Management System",background:"pension-management.webp",github:"https://github.com/Shuktika15/pension-management",descriptions:[`A full stack application to calculate pension given the Aadhar Number of a pensioner. 
                 Backend uses Microservices architecture provided by Spring Cloud. Also uses Service Discovery, 
                 Feign Client, API Gateway, JWT based authorization to structure the application and 
                 containerized with Docker. Used H2 as database. Frontend utilizes Angular Material to create the UI. 
